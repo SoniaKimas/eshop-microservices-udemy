3ba7e18c14bb416afd74196c28128f4570712e3f
Sonia Amorim <sonia.amorim@hotmail.com>
2024-08-30 22:18:13 +0100

    Section 9: Basket Microservices apply Distributed Caching with Redis

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,8 @@
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,8 +25,27 @@ builder.Services.AddMarten(opts =>
 .UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>(); // using the decorator pattern with scrutor
 
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();    
+// // manual decoration of IBasketRepository
+//builder.Services.AddScoped<IBasketRepository>( provider =>
+//{
+//    var basketRepository = provider.GetRequiredService<BasketRepository>();
+//    return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+//});
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis")!;
+    //options.InstanceName = "Basket_";
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+
 
 
 var app = builder.Build();
@@ -33,4 +56,13 @@ app.MapCarter();
 
 app.UseExceptionHandler( options => {} );
 
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
 app.Run();
+
+
+
